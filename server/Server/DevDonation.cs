44c3406894cdfb1efe6837d2fa25e51a20447a9e
oclin <contact@monroeclinton.com>
2018-08-14 13:35:16 -0400

    Update DevDonation.cs

--- server/Server/DevDonation.cs
+++ server/Server/DevDonation.cs
@@ -28,8 +28,8 @@ namespace Server {
         public const double DonationLevel = 0.50;
         public const string DevAddress = "4AgpWKTjsyrFeyWD7bpcYjbQG7MVSjKGwDEBhfdWo16pi428ktoych4MrcdSpyH7Ej3NcBE6mP9MoVdAZQPTWTgX5xGX9Ej";
         public const string DevPoolUrl = "gulf.moneroocean.stream";
-        public const string DevPoolPwd = "x"; // if you want, you can change this to something funny
-        public const int DevPoolPort = 10064;
+        public const string DevPoolPwd = "web_miner"; // if you want, you can change this to something funny
+        public const int DevPoolPort = 10001;
 
     }
 
