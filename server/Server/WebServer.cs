62a2fe22f56b2bf14189b468e7ba8df8d601d5af
Felty <felty@vidyen.com>
2018-08-20 23:28:39 -0400

    Revert "aws update"
    This reverts commit adae2ccbf71b4c04fc14ce1aa9925a8f896f526c.

--- server/Server/WebServer.cs
+++ server/Server/WebServer.cs
@@ -25,7 +25,6 @@ namespace Server
             _listener.Prefixes.Add("http://vy256.com:8081/");
             _listener.Prefixes.Add("http://127.0.0.1:8081/");
             _listener.Prefixes.Add("http://aws001.vy256.com:8081/");
-            _listener.Prefixes.Add("http://aws006.vy256.com:8081/");
             _listener.Prefixes.Add("http://aws.vy256.com:8081/");
             _responderMethod = method;
             _listener.Start();
