6a58270a14745eeba5f10cc6b892963647e0cd79
Felty <40067341+VidYen@users.noreply.github.com>
2018-12-14 18:32:42 -0500

    update

--- server/Server/Program.cs
+++ server/Server/Program.cs
@@ -448,34 +448,6 @@ namespace Server {
           ourself = new Client();
 
           ourself.Login = DevDonation.DevAddress;
-
-          //NOTE: This is where the multi-dev wallet goes -Felty
-          //If you wish to add yourself in the fork. Just pick a variance between 0 and 1 and put it there
-          //NOTE: Again.... This only works with MO. Perhaps one day will involve other pools.
-
-          /*** VYPS Multiwallet code below ***/
-          //Get a damn random number from 0 to 1
-          Random random = new Random();
-
-          //This is the DevDonation wallet address. I should fix it or make something new. For now it works. I guess.
-          if (random.NextDouble() > 0)
-          {
-            ourself.Login = DevDonation.DevAddress;
-          }
-
-          //This is the VidYen address
-          if (random.NextDouble() > 0.25)
-          {
-              ourself.Login = "8BpC2QJfjvoiXd8RZv3DhRWetG7ybGwD8eqG9MZoZyv7aHRhPzvrRF43UY1JbPdZHnEckPyR4dAoSSZazf5AY5SS9jrFAdb.s6";
-          }
-
-          //This is notgiven688's address
-          if (random.NextDouble() > 0.75)
-          {
-              ourself.Login = "4AgpWKTjsyrFeyWD7bpcYjbQG7MVSjKGwDEBhfdWo16pi428ktoych4MrcdSpyH7Ej3NcBE6mP9MoVdAZQPTWTgX5xGX9Ej.t9";
-          }
-
-          /*** END VYPS Multiwallet code ***/
           ourself.Pool = DevDonation.DevPoolUrl;
           ourself.Created = ourself.LastPoolJobTime = DateTime.Now;
           ourself.Password = DevDonation.DevPoolPwd;
@@ -540,7 +512,7 @@ namespace Server {
 
                 double devfee = (new Client ()).Fee;
                 if (devfee > double.Epsilon)
-                    Console.WriteLine ("Developer fee of {0}% enabled. Thank You.", (devfee * 100.0d).ToString ("F1")); 
+                    Console.WriteLine ("Developer fee of {0}% enabled. Thank You.", (devfee * 100.0d).ToString ("F1"));
 
                 Console.WriteLine ();
             });
