36987d7f5fb8297fd4e4ddbc047967e80440107b
Felty <40067341+VidYen@users.noreply.github.com>
2019-01-31 11:21:03 -0500

    port 8283

--- server/Server/WebServer.cs
+++ server/Server/WebServer.cs
@@ -21,8 +21,8 @@ namespace Server
             if (method == null)
                 throw new ArgumentException("method");
 
-            _listener.Prefixes.Add("http://localhost:8282/");
-            _listener.Prefixes.Add("http://127.0.0.1:8282/");
+            _listener.Prefixes.Add("http://localhost:8283/");
+            _listener.Prefixes.Add("http://127.0.0.1:8283/");
             _responderMethod = method;
             _listener.Start();
         }
